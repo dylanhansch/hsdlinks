e6cd4c19b79451bb7e83120e7d2a56d7f22a6e42
Dylan Hansch <dylan@dylanhansch.net>
2014-10-13 13:45:25 -0500

    Restructure

---
+++ src/install.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Install HSDLinks</title>
+		<meta charset="utf-8">
+		<meta name="author" content="Dylan Hansch">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<link rel="shortcut icon" content="none">
+		
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	</head>
+	<body>
+		<?php include("navbar.php"); ?>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12" style="padding-top:50px;">
+					<h1>Install HSDLinks <small>Please delete this PHP document (install.php) after you've installed HSDLinks</small></h1>
+					<p>HSDLinks is a private URL shortener made by Dylan Hansch. It's open source, so feel free to view the code <a href="https://github.com/dylanhansch/hsdlinks">here</a>.</p>
+					
+					<h2>Step 1. Create a database</h2>
+					
+					
+				</div>
+			</div>
+		</div>
+		
+		<?php include("footer.php"); ?>
+	</body>
+</html>
